736570ed20de83c9dab07fb4ef9a216458f39ff7
Haneen  Kamel <116191117+haneenkamel2001@users.noreply.github.com>
2023-04-09 15:02:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+var app = builder.Build();
+app.Urls.Add("http://localhost:5000");
+
+// Middlewares
+
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/ucas", () => "ucas.edu.ps");
+app.MapGet("/coddict", () => new
+{
+    Name = "City",
+    Url = "https://coddict.co",
+    Started = 4000
+});
+
+app.MapPost("/students", () => "Student Created!");
+
+app.Run();
+
